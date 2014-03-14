5b4cd168d39951583547b62fe8788ad0c974fa5c
Francois Lacoursiere <flacours@gmail.com>
2014-03-14 12:34:19 -0400

    part 2 squeleton

---
+++ SourceFiles/Skeleton/iRememberContentProvider/iRememberContentProvider/gen/edu/vanderbilt/mooc/mooc_cp/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.vanderbilt.mooc.mooc_cp;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
